adb4036ae8a0a1a1dae2feee55b4c84c1ea7f926
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-06 21:16:29 -0800

    adjusted index.js to a class component and added hover checks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
@@ -7,10 +7,37 @@ import Header from '../components/header'
 import Image from '../components/image'
 import Card from '../components/card'
 
-const IndexPage = () => (
-  <div>
-    <Card />
-  </div>
-)
+// const IndexPage = () => (
+//   <div>
+//     <Card />
+//   </div>
+// )
 
-export default IndexPage
+// export default IndexPage
+
+export default class IndexPage extends Component {
+  state = {
+    hover: false,
+  }
+  onHover = () => {
+    this.setState(state => {
+      return {
+        hover: !state.hover,
+      }
+    })
+  }
+
+  render() {
+    const { hover } = this.state
+
+    return (
+      <Fragment>
+        <Card
+          onMouseEnter={() => this.onHover()}
+          onMouseLeave={() => this.onHover()}
+        />
+        <p>{JSON.stringify(hover)}</p>
+      </Fragment>
+    )
+  }
+}
