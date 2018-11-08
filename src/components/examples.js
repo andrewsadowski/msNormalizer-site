c763d104f2f5f8660ba288e7da7ec42ba8e64c0f
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-07 19:50:41 -0800

    added styled components for examples component

--- src/components/examples.js
+++ src/components/examples.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import GifDir from '../images/msNorm_dir.gif'
+import GifSingle from '../images/msNorm_singleFile.gif'
+
+const ExampleContainer = styled.div`
+  display: flex;
+  margin-top: 100px;
+  flex-direction: column;
+`
+
+const ExampleGif = styled.img``
 
 export default class Examples extends Component {
   render() {
     return (
-      <div>
+      <ExampleContainer>
         <p>Examples</p>
-      </div>
+        <ExampleGif src={GifSingle} />
+        <ExampleGif src={GifDir} />
+      </ExampleContainer>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 
 import Card from '../components/card'
+import Examples from '../components/examples'
 
 export default class IndexPage extends Component {
   render() {
     return (
       <Fragment>
         <Card />
+        <Examples />
       </Fragment>
     )
   }
