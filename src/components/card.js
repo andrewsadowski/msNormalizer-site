4992cde9eda746e116ab79f1cf549354a2defcdc
admin <andrew.n.sadowski@gmail.com>
2018-11-07 16:56:53 -0800

    removed hover rules in css for CardContainer styled component

--- src/components/card.js
+++ src/components/card.js
@@ -15,11 +15,6 @@ const CardContainer = styled.div`
   max-width: 500px;
   margin: auto;
   margin-top: 75px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-
-  &:hover {
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  }
 `
 
 export default class Card extends Component {
