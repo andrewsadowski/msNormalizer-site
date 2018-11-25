03c462574ed00ed78692d6127a862b698304b06b
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-25 12:42:09 -0800

    added github image, created styled Image and Container components for sizing of images

--- src/components/card.js
+++ src/components/card.js
@@ -5,6 +5,7 @@ import { Spring } from 'react-spring'
 import Info from './info'
 import MSImg from '../images/msNormalizer.png'
 import NPMImg from '../images/npm_logo.svg'
+import GHImg from '../images/GitHub_Logo.png'
 
 const CardContainer = styled.div`
   display: flex;
@@ -16,6 +17,20 @@ const CardContainer = styled.div`
   max-width: 500px;
   margin: auto;
   margin-top: 75px;
+`
+const ImageContainer = styled.div`
+  display: flex;
+  justify-items: row;
+`
+
+const Image = styled.img`
+  padding-left: 10px;
+  padding: 10px;
+  margin: auto;
+  max-width: 100px;
+  max-height: 100px;
+  height: 50px;
+  width: auto;
 `
 
 export default class Card extends Component {
@@ -48,7 +63,14 @@ export default class Card extends Component {
             >
               <img src={MSImg} />
               <Info />
-              {/* <img src={NPMImg} /> */}
+              <ImageContainer>
+                <a href="https://www.npmjs.com/package/srt-ms-normalizer">
+                  <Image src={NPMImg} alt="npm-link" />
+                </a>
+                <a href="https://github.com/andrewsadowski/SRT-Millisecond-Normalizer">
+                  <Image src={GHImg} alt="github-link" />
+                </a>
+              </ImageContainer>
             </CardContainer>
           )}
         </Spring>
