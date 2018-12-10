201131837ee80e6365f1156362ed7f73fe1a9cd7
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-09 16:47:28 -0800

    added styledcomponent for Footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,20 @@
 import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 
+const FooterSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 export default class Footer extends Component {
   render() {
     return (
       <Fragment>
-        <div>
+        <FooterSection>
           <p>Goodbye!</p>
-        </div>
+        </FooterSection>
       </Fragment>
     )
   }
