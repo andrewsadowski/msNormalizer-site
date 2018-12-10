3a8eea163e36ec95ea2235ccf30c864ed3634134
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-09 16:22:56 -0800

    added githubRibbon component

---
+++ src/components/githubRibbon.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+import GithubCorner from 'react-github-corner'
+
+export default class GithubRibbon extends Component {
+  render() {
+    return (
+      <Fragment>
+        <GithubCorner href="https://github.com/andrewsadowski/SRT-Millisecond-Normalizer" />
+      </Fragment>
+    )
+  }
+}
