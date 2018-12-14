45c67c96429bf03eb12694b28b7b203748810e93
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-13 19:49:33 -0800

    adjusted the spring on card component to be more obvious

--- src/components/card.js
+++ src/components/card.js
@@ -69,7 +69,7 @@ export default class Card extends Component {
         <Spring
           to={{
             boxShadow: hover
-              ? '0 3px 6px rgba(0, 0, 0, 0.20), 0 3px 6px rgba(0, 0, 0, 0.25)'
+              ? '0 3px 6px rgba(0, 0, 0, 0.20), 0 10px 15px rgba(0, 0, 0, 0.25)'
               : '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
           }}
         >
