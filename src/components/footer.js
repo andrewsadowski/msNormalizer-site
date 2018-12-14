881fb640bb71e87e06f08631fdc9b5509fe7d159
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-13 19:36:40 -0800

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,17 +10,25 @@ const FooterSection = styled.div`
 
 const FooterText = styled.p`
   font-family: 'Roboto', sans-serif;
+  margin-top: 20px;
   word-break: auto;
   font-size: 15px;
   /* width: 200px; */
 `
+
+const SpecializedLink = styled.a``
 
 export default class Footer extends Component {
   render() {
     return (
       <Fragment>
         <FooterSection>
-          <FooterText>Goodbye!</FooterText>
+          <FooterText>
+            Tell me what you think{' '}
+            <a href="https://github.com/andrewsadowski/SRT-Millisecond-Normalizer/issues">
+              here
+            </a>
+          </FooterText>
         </FooterSection>
       </Fragment>
     )
