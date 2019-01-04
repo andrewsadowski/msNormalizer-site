c911773db5ae446f32fef56bcce04cc0484102b9
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2019-01-04 01:37:05 +0100

    removed gatsby-plugin-remove-serviceWorker from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-styled-components',
-    `gatsby-plugin-remove-serviceworker`,
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
